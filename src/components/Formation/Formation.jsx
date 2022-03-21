c06dac3850cc7ba700fa1e8221505a29d31aef1d
nicolas-saradin <saradinnicolas9@gmail.com>
2022-03-21 14:50:29 +0100

    Menu Formation

---
+++ src/components/Formation/Formation.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Formation.css';
+
+function Formation() {
+  return (
+    <div className="container">
+      <div className="child1">
+        <div className="Image_Container">
+          <img src="#" alt="PROGRESSION DE DESSIN" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">PROGRESSION DE DESSIN</button>
+        </div>
+      </div>
+
+      <div className="child2">
+        <div className="Image_Container">
+          <img src="#" alt="PROGRESSION D'ATELIER" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">PROGRESSION D'ATELIER</button>
+        </div>
+      </div>
+
+      <div className="child3">
+        <div className="Image_Container">
+          <img src="#" alt="MINI-STAGE" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">MINI-STAGE</button>
+        </div>
+      </div>
+
+      <div className="child4">
+        <div className="Image_Container">
+          <img src="#" alt="PROGRESSION D'ATELIER" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">PROGRESSION D'ATELIER</button>
+        </div>
+      </div>
+
+      <div className="child5">
+        <div className="Image_Container">
+          <img src="#" alt="PARCOUR DE FORMATION" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">PARCOUR DE FORMATION</button>
+        </div>
+      </div>
+
+      <div className="child6">
+        <div className="Image_Container">
+          <img src="#" alt="ARCHIVE" />
+        </div>
+        <div className="BTN">
+          <button className="formationBtn">ARCHIVE</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Formation;
