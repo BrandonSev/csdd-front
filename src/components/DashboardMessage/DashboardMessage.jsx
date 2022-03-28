7f408911636c30d38ac7fadb063d14f294b59df7
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-03-28 10:59:04 +0200

    Component DashboardMessage et MessageList

--- src/components/DashboardMessage/DashboardMessage.jsx
+++ src/components/DashboardMessage/DashboardMessage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BiUserPlus } from 'react-icons/bi';
+import { BiEnvelope } from 'react-icons/bi';
 import Dashboard from '../Dashboard';
 import DashboardMenu from '../Dashboard/DashboardMenu';
 import DashboardHeader from '../Dashboard/DashboardHeader';
 import DashboardBody from '../Dashboard/DashboardBody';
+import MessageList from '../MessageList/MessageList';
 
 import './DashboardMessage.css';
 
@@ -13,13 +14,15 @@ function DashboardMessage() {
       <DashboardMenu />
       <DashboardHeader />
       <DashboardBody>
-        <div className="container-message">
-          <h1 className="avatar-name">
-            <BiUserPlus size={40} /> Prénom NOM
-          </h1>{' '}
-          <br />
-          <p>Prénom NOM a fait une demande d'accès à la plateforme</p> <br />
-          <div className="datetime">Reçu le : datetime-format</div>
+        <h1 className="title-page-message">
+          Liste des messages <BiEnvelope size={40} />
+        </h1>
+        <div className="dashboard-message">
+          <MessageList />
+          <MessageList />
+          <MessageList />
+          <MessageList />
+          <MessageList />
         </div>
       </DashboardBody>
     </Dashboard>
