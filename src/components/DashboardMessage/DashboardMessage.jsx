a0dc6ef080d78a84ca191956c002632aa33efa1f
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-04-12 17:18:24 +0200

    Icon enveloppe in the center

--- src/components/DashboardMessage/DashboardMessage.jsx
+++ src/components/DashboardMessage/DashboardMessage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BiEnvelope } from 'react-icons/bi';
+import { IconContext } from 'react-icons';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import MessageList from '../MessageList/MessageList';
@@ -20,14 +21,19 @@ function DashboardMessage() {
   }, []);
   return (
     <>
-        <h1 className="title-page-message">
-          Liste des messages <BiEnvelope size={40} />
-        </h1>
-        <div className="dashboard-message">
-          {message?.map((message) => (
-            <MessageList {...message} />
-          ))}
-        </div>
+      <h1 className="title-page-message">
+        Liste des messages{' '}
+        <IconContext.Provider
+          value={{ size: 40, style: { verticalAlign: 'middle' } }}
+        >
+          <BiEnvelope />
+        </IconContext.Provider>
+      </h1>
+      <div className="dashboard-message">
+        {message?.map((message) => (
+          <MessageList {...message} />
+        ))}
+      </div>
     </>
   );
 }
