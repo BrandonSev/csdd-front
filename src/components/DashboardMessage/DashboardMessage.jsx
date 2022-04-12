0610ac725a635f425a84e837e9dbe576bcbbd9d7
BrandonSev <seveste.brandon@gmail.com>
2022-04-12 16:08:56 +0200

    refactoring dashboard structure + error user dashboard

--- src/components/DashboardMessage/DashboardMessage.jsx
+++ src/components/DashboardMessage/DashboardMessage.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import { BiEnvelope } from 'react-icons/bi';
-import Dashboard from '../Dashboard';
-import DashboardMenu from '../Dashboard/DashboardMenu';
-import DashboardHeader from '../Dashboard/DashboardHeader';
-import DashboardBody from '../Dashboard/DashboardBody';
 import MessageList from '../MessageList/MessageList';
 
 import './DashboardMessage.css';
 
 function DashboardMessage() {
   return (
-    <Dashboard>
-      <DashboardMenu />
-      <DashboardHeader />
-      <DashboardBody>
-        <h1 className="title-page-message">
-          Liste des messages <BiEnvelope size={40} />
-        </h1>
-        <div className="dashboard-message">
-          <MessageList />
-          <MessageList />
-          <MessageList />
-          <MessageList />
-          <MessageList />
-        </div>
-      </DashboardBody>
-    </Dashboard>
+    <>
+      <h1 className="title-page-message">
+        Liste des messages <BiEnvelope size={40} />
+      </h1>
+      <div className="dashboard-message">
+        <MessageList />
+        <MessageList />
+        <MessageList />
+        <MessageList />
+        <MessageList />
+      </div>
+    </>
   );
 }
 
