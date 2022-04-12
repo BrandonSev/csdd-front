3c0a54c060ff6b53a30296b448879fd33e6343fb
nicolas-saradin <saradinnicolas9@gmail.com>
2022-04-12 11:03:25 +0200

    réadaptation du css

--- src/components/SitePages/VieDeLaCorpo/Corpo.jsx
+++ src/components/SitePages/VieDeLaCorpo/Corpo.jsx
@@ -7,66 +7,68 @@ import reunionsNationales from '../../../assets/reunionsNationales.png';
 
 function VieDeLaCorpo() {
   return (
-    <div className="container formation-container">
+    <div className="">
       <h1 className="TitleForMobile">Vie de la Corpo</h1>
-      <div className="child">
-        <div className="formation-image-container">
-          <img className="formation-image" src={annuaire} alt="ANNUAIRE" />
-        </div>
-        <div className="formation-btn-container">
-          <div>
-            <Button className="formation-btn" buttonName="ANNUAIRE" />
+      <div className="container formation-container">
+        <div className="child">
+          <div className="formation-image-container">
+            <img className="formation-image" src={annuaire} alt="ANNUAIRE" />
+          </div>
+          <div className="formation-btn-container">
+            <div>
+              <Button className="formation-btn" buttonName="ANNUAIRE" />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="child">
-        <div className="formation-image-container">
-          <img
-            className="formation-image"
-            src={organigramme}
-            alt="ORGANIGRAMME"
-          />
-        </div>
-        <div className="formation-btn-container">
-          <div>
-            <Button className="formation-btn" buttonName="ORGANIGRAMME" />
-          </div>
-        </div>
-      </div>
-
-      <div className="child">
-        <div className="formation-image-container">
-          <img
-            className="formation-image"
-            src={reunionsNationales}
-            alt="Réunions nationales"
-          />
-        </div>
-        <div className="formation-btn-container">
-          <div>
-            <Button
-              className="formation-btn"
-              buttonName="REUNIONS NATIONALES"
+        <div className="child">
+          <div className="formation-image-container">
+            <img
+              className="formation-image"
+              src={organigramme}
+              alt="ORGANIGRAMME"
             />
           </div>
+          <div className="formation-btn-container">
+            <div>
+              <Button className="formation-btn" buttonName="ORGANIGRAMME" />
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="child">
-        <div className="formation-image-container">
-          <img
-            className="formation-image"
-            src={regleCorporative}
-            alt="REGLES CORPORATIVE"
-          />
-        </div>
-        <div className="formation-btn-container">
-          <div>
-            <Button
-              className="formation-btn"
-              buttonName="REGLES CORPORATIVES"
+        <div className="child">
+          <div className="formation-image-container">
+            <img
+              className="formation-image"
+              src={reunionsNationales}
+              alt="Réunions nationales"
             />
+          </div>
+          <div className="formation-btn-container">
+            <div>
+              <Button
+                className="formation-btn"
+                buttonName="REUNIONS NATIONALES"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="child">
+          <div className="formation-image-container">
+            <img
+              className="formation-image"
+              src={regleCorporative}
+              alt="REGLES CORPORATIVE"
+            />
+          </div>
+          <div className="formation-btn-container">
+            <div>
+              <Button
+                className="formation-btn"
+                buttonName="REGLES CORPORATIVES"
+              />
+            </div>
           </div>
         </div>
       </div>
